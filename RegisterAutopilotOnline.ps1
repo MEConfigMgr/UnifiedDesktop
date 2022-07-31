297f6a01a334c5ad0701a4e1d80f9ba79a0236b9
MEConfigMgr <61551639+MEConfigMgr@users.noreply.github.com>
2022-07-31 15:15:31 -0500

    Add files via upload

---
+++ RegisterAutopilotOnline.ps1
@@ -0,0 +1,5 @@
+﻿Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned -Confirm:$false -Force:$true
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet
+Install-Script -Name Get-WindowsAutopilotInfo -Confirm:$false -Force:$true
+Get-WindowsAutoPilotInfo.ps1 -Online
